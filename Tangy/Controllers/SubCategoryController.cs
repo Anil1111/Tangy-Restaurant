9b4cba3687f0a9bfabaa8ad3cd5d648154bb726c
Joshua Tucker <thejltucker@gmail.com>
2019-04-01 15:26:48 -0400

    Started work on SubCategory Models/Controller

---
+++ Tangy/Controllers/SubCategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tangy.Data;
+
+namespace Tangy.Controllers
+{
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SubCategoryController(ApplicationDbContext db) => _db = db;
+
+        // GET SubCategory
+        public async Task<IActionResult> Index()
+        {
+            var subCategories = _db.SubCategories.Include(s => s.Category);
+
+            return View(await subCategories.ToListAsync());
+        }
+    }
+}
